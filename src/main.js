2f442411d16ac79d76022054d776e1cfb5ec830e
Tony Yip <tony@opensource.hk>
2017-10-10 01:02:34 +0800

    Add ga tracking

--- src/main.js
+++ src/main.js
@@ -2,6 +2,11 @@ import Vue from 'vue';
 import Vuetify from 'vuetify';
 import App from './App.vue';
 
+/* globals window */
+window.ga('create', 'UA-103662489-1', 'auto');
+window.ga('set', 'page', `${window.location.hostname}${window.location.pathname}`);
+window.ga('send', 'pageview');
+
 Vue.use(Vuetify);
 
 Vue.config.productionTip = false;
